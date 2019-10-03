dcc895f8320dfb673de858fe7732aeb691609b9d
Julianjuaner <julianjuaner@gmail.com>
2019-10-03 21:24:09 +0800

    K-pop easy extract

--- kichu.py
+++ kichu.py
@@ -5,11 +5,12 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import requests
+import xlsxwriter 
 import time
 import sys
 import re
 import json
-import MySQLdb
+
 
 def load_pts_data(file_name):
     already = 148230
@@ -40,22 +41,69 @@ def load_pts_data(file_name):
                                     row['likes'], row['keywords'], row['pts']))
     
 
+aid = np.empty(0)
+view = np.empty(0)
+cid = np.empty(0)
+retry = False
+head = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+    }
+
+
+def spider(url):
+    workbook = xlsxwriter.Workbook('pop.xlsx')
+    worksheet = workbook.add_worksheet()
+    global retry, aid, view, cid
+    print(url)
+    try:
+        JSON = requests.get(url, headers = head, timeout=(10, 10)).text
+    except Exception as e:
+        print(e)
+        if retry == False:
+            spider(url)
+            retry = True
+            return
+        return
+    retry = False
+
+    JSON = json.loads(JSON)
+    #print(json.dumps(JSON, indent=4, ensure_ascii=False))
+    time.sleep(0.1)
+    #print(url)
+    if JSON["code"] == 0:
+        #video_counter += 1
+        #print(video_counter, end='')
+        content = JSON["data"]["archives"]
+        for m in content:
+            aid = np.append(aid, m['aid'])
+            view = np.append(view, m['stat']['view'])
+            cid = np.append(cid, m['owner']['mid'])
+
+        for it in range(len(aid)):
+            worksheet.write(it, 0, aid[it])
+            worksheet.write(it, 1, cid[it])
+            worksheet.write(it, 2, view[it])
+
+        aid = np.empty(0)
+        view = np.empty(0)
+        cid = np.empty(0)
+        workbook.close()
+
 def video_av_extract():
     i=1
     urls = []
-    tD='https://api.bilibili.com/x/web-interface/newlist?callback=jqueryCallback_bili_8285897583458257&rid=22&type=0&pn='+str(i)+'&ps=20'
+    zongyi='https://api.bilibili.com/x/web-interface/newlist?callback=jqueryCallback_bili_4112257624761402&rid=71&type=0&pn=' +str(i)+'&ps=20'
+    star = 'https://api.bilibili.com/x/web-interface/newlist?callback=jqueryCallback_bili_022573794559991534&rid=137&type=0&pn=' +str(i)+'&ps=20'
+
     tooD='https://api.bilibili.com/x/web-interface/newlist?callback=jqueryCallback_bili_20679987710906045&rid=26&type=0&pn='+str(i)+'&ps=20'
     VOCALOID='https://api.bilibili.com/x/web-interface/newlist?callback=jqueryCallback_bili_6060373409639355&rid=126&type=0&pn='+str(i)+'&ps=20'
     guide='https://api.bilibili.com/x/web-interface/newlist?callback=jqueryCallback_bili_08424519499179617&rid=127&type=0&pn='+str(i)+'&ps=20'
-    head = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-    }
 
     time1 = time.time()
     #video_counter = 547653
     #48010
-    for i in range(0, 58):
-        url ='https://api.bilibili.com/x/web-interface/newlist?callback=jqueryCallback_bili_08424519499179617&rid=127&type=0&pn='+str(i)+'&ps=20'
+    for i in range(10000, 20000):
+        url ='https://api.bilibili.com/x/web-interface/newlist?callback=jqueryCallback_bili_4112257624761402&rid=71&type=0&pn=' +str(i)+'&ps=20'
         urls.append(url)
     print('url loaded.')
     #'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36'
@@ -63,42 +111,19 @@ def video_av_extract():
     print("sleeping")
     #time.sleep(500)
     print("over")
-    retry = False
     Validtype = ['2dMAD', 'kichiku', 'VOCALOID', 'guide', '3dMAD']
-    a = np.empty(0)
-    def spider(url):
-        print(url)
-        global retry, a
-        try:
-            JSON = requests.get(url, headers = head, timeout=(10, 10)).text
-        except Exception as e:
-            print(e)
-            if retry == False:
-                spider(url)
-                retry = True
-                return
-            return
-        retry = False
 
-        JSON = json.loads(JSON)
-        #print(json.dumps(JSON, indent=4, ensure_ascii=False))
-        time.sleep(0.13)
-        #print(url)
-        if JSON["code"] == 0:
-            #video_counter += 1
-            #print(video_counter, end='')
-            content = JSON["data"]["archives"]
-            for m in content:
-                a = np.append(a, m['aid'])
                 #print(a)
     #pool = ThreadPool()
     # results = pool.map(spider, urls)
     #print('here!')
     for m in urls:
         spider(m)
-    np.save("guide.npy",a) 
+
+    np.save("./videoid/pop.npy", aid) 
         #print('here!')
 
 if __name__ == '__main__':
-    load_pts_data('data_pts.xlsx')
+    video_av_extract()
+    #load_pts_data('data_pts.xlsx')
 
